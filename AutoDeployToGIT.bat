0cb87eacb6c9d7de8f138bc0857838e3f6072469
chungminhtu <chungminhtu@gmail.com>
2018-10-19 13:28:37 +0700

    2018-10-19_0128 PM

---
+++ AutoDeployToGIT.bat
@@ -0,0 +1,8 @@
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+hugo
+cd public
+git add .
+git commit -m "%mydate%_%mytime%"
+git push origin master
